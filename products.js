204f457268a7b77c1c5878e70f29b30ccdc188af
amineayman0 <106935462+amineayman0@users.noreply.github.com>
2022-06-27 00:14:16 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,18 @@
+const products = [
+	{
+		id: 2,
+		logo:'img/logo.png',
+		image: 'img/products/1.png',
+		image2: 'img/products/2.png',
+		image3: 'img/products/3.png',
+		image4: 'img/products/4.png',
+		image5: 'img/products/5.png',
+		title: '   مضخة المياه من كراون لضخ المياه بقوة ضغط عالية ',
+		price1: 14000,
+		price: 10200,
+		description: 'محرك أوتوماتيكي أصلي من كراون لضخ المياه بقوة ضغط عالية Crown Pompe à Eau & Cerveau Suppresseur CT35045 تعد شركة CROWN الأمريكية الرائدة في مجال الصناعة والمعدات الصناعية والحرفية واحدة من أفضل خمس شركات عالمية في هذا المجال، اكتسبت سمعة كبيرة كمبتكر عالمي وتلقت العديد من الجوائز الدولية، هذا لسعيها الدائم في دمج أحدث وأفضل التكنولوجيات في تصميم منتجاتها، مع اهتمامها بسلامة وصحة المستخدمين. Voltage rating: 220V/50Hz. Rated Power: 370W. Max Current: 2.5 A Flow: 35L/min. Lift 42 M. The Absorption Process: 8 M.'   ,
+		phone: 0540643397,
+	},
+
+
+];
